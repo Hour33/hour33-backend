15c6eec039f344a834c43b579c78d7478b1dab6f
tasks222 <leewandee11@gmail.com>
2026-03-10 03:08:03 -0400

    hour33 backend first upload

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require("express")
+const cors = require("cors")
+const sqlite3 = require("sqlite3").verbose()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const db = new sqlite3.Database("./users.db")
+
+db.run(
+CREATE TABLE IF NOT EXISTS users(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+phone TEXT UNIQUE,
+password TEXT,
+balance INTEGER
+)
+)
+
+// REGISTER
+app.post("/register",(req,res)=>{
+
+const {phone,password}=req.body
+
+db.get("SELECT * FROM users WHERE phone=?",[phone],(err,row)=>{
+
+if(row){
+return res.json({success:false,message:"Account exists"})
+}
+
+db.run(
+"INSERT INTO users(phone,password,balance) VALUES(?,?,?)",
+[phone,password,200],
+err=>{
+
+if(err){
+return res.json({success:false})
+}
+
+res.json({success:true})
+
+}
+)
+
+})
+
+})
+
+// LOGIN
+app.post("/login",(req,res)=>{
+
+const {phone,password}=req.body
+
+db.get("SELECT * FROM users WHERE phone=?",[phone],(err,user)=>{
+
+if(!user){
+return res.json({success:false,message:"No account"})
+}
+
+if(user.password!==password){
+return res.json({success:false,message:"Wrong password"})
+}
+
+res.json({success:true,user})
+
+})
+
+})
+
+app.listen(3000,()=>{
+console.log("Server running on port 3000")
+})
